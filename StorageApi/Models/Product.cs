48b9986df65cefd5368b7085b4a19597edddc124
Albin <albin@bohlin.nu>
2025-06-28 00:04:06 +0200

    product.cs

---
+++ StorageApi/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿namespace StorageApi.Models
+{
+
+
+
+    /*Del 2 – Skapa din modell
+1. Skapa en mapp Models
+2. Skapa en klass Product.cs med följande egenskaper:
+o int Id,
+o string Name,
+o int Price,
+o string Category,
+o string Shelf,
+o int Count,
+o string Description,
+    */
+
+
+
+
+    public class Product
+    {
+        public Product(int Id,
+                       string Name,
+                       int Price,
+                       string Category,
+                       string Shelf,
+                       int Count,
+                       string Description
+
+            )
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Price = Price;
+            this.Category = Category;
+            this.Shelf = Shelf;
+            this.Count = Count;
+            this.Description = Description;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public int Price { get; }
+        public string Category { get; }
+        public string Shelf { get; }
+        public int Count { get; }
+        public string Description { get; }
+    }
+}
