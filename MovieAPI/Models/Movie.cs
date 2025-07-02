ecc1831a0add996a6fc8bc5a1dbf5548c6c1afc6
Albin <albin@bohlin.nu>
2025-07-02 12:38:14 +0200

    before first DB

---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using MovieAPI.Models;
+
+namespace MovieAPI.Items
+{
+    public class Movie
+    {
+        public int Id{ get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public TimeSpan Duration { get; set; } = TimeSpan.FromMinutes(Random.Shared.Next(80,240));
+
+        public List<Review> reviews { get; set; } = []; 
+
+    }
+}
---
+++ MovieAPI/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieAPI.Models
+
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewerName { get; set; }
+        public int Rating { get; set; } = Random.Shared.Next(1,6);
+        public string Comment { get; set; }
+    }
+}
